88953fd8e54e1bbcfb475b1b8a8ec7258036836c
IT22056320 <127720899+IT22056320@users.noreply.github.com>
2023-03-17 17:09:46 +0000

    Completed Tute03

--- .ccls-cache/@home@runner@tutorial-01-IT22056320/Tute03.c
+++ .ccls-cache/@home@runner@tutorial-01-IT22056320/Tute03.c
@@ -13,6 +13,15 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+  int n,sum=0;
+
+  printf("Enter Positive Integer n:");
+  scanf("%d",&n);
+
+  for(int i=0;i<=n;i++){
+    sum+=i;
+  }
+  printf("The sum of numbers from 1 to %d is %d\n",n,sum);
   
   return 0;
 }
